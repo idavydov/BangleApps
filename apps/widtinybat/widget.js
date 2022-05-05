2bcb41fa63886db10beb33879f9a9e5e64ffdb0c
Iakov Davydov <idavydov@users.noreply.github.com>
2022-05-05 21:02:35 +0200

    wid_tinybat

---
+++ apps/widtinybat/widget.js
@@ -0,0 +1,51 @@
+(() => {
+  const intervalLow = 60000; // update time when not charging
+  const intervalHigh = 2000; // update time when charging
+  const outline = atob("CRSBAD4AP/AYDAYDAYDAYDAYDAYDAYDAYD/w");
+
+  let COLORS = {
+    'black':    g.theme.dark ? "#fff" : "#000",
+    'charging': "#0f0",
+    'low':      "#f00",
+  };
+
+  function draw() {
+    var i;
+    var ocol = COLORS.low;
+    var ccol = COLORS.low;
+    var ncells = 0;
+
+    const bat = E.getBattery();
+    if (bat > 5) {
+      ocol = COLORS.black;
+      ncells = 1 + Math.floor((bat-6)/19);
+    }
+    if (ncells > 1) {
+      ccol = COLORS.black;
+    }
+    if (Bangle.isCharging()) {
+      ocol = COLORS.charging;
+    }
+    g.reset();
+    g.setColor(ocol).drawImage(outline,this.x+2,this.y);
+    for (i = 0; i < ncells; i++) {
+      var x = this.x + 2 + 2;
+      var y = this.y + 16 - i * 3;
+      g.setColor(ccol).drawRect(x, y, x + 4, y + 1);
+    }
+    if (Bangle.isCharging()) {
+      changeInterval(id, intervalHigh);
+    } else {
+      changeInterval(id, intervalLow);
+    }
+  }
+
+  Bangle.on('charging', function(charging) { draw(); });
+  var id = setInterval(()=>WIDGETS["wid_tinybat"].draw(), intervalLow);
+
+  WIDGETS["wid_tinybat"]={
+    area:"tr",
+    width: 13,
+    draw:draw
+  };
+})();
