49817b8d71c3e2b8f56417937b28ee37ec581de3
Iakov Davydov <idavydov@users.noreply.github.com>
2022-05-13 15:31:13 +0200

    widtbt: settings to hide when no connection

--- apps/widbt/widget.js
+++ apps/widbt/widget.js
@@ -1,4 +1,6 @@
-WIDGETS["bluetooth"]={area:"tr",width:15,draw:function() {
+WIDGETS["bluetooth"]={area:"tr",draw:function() {
+  if (WIDGETS["bluetooth"].width==0)
+    return;
   g.reset();
   if (NRF.getSecurityStatus().connected)
     g.setColor((g.getBPP()>8) ? "#07f" : (g.theme.dark ? "#0ff" : "#00f"));
@@ -6,7 +8,16 @@ WIDGETS["bluetooth"]={area:"tr",width:15,draw:function() {
     g.setColor(g.theme.dark ? "#666" : "#999");
   g.drawImage(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="),2+this.x,2+this.y);
 },changed:function() {
+  WIDGETS["bluetooth"].width = WIDGETS["bluetooth"].show()?15:0;
   WIDGETS["bluetooth"].draw();
+},show:function() {
+  if (NRF.getSecurityStatus().connected)
+    return true;
+  var settings = require('Storage').readJSON("widbt.json", true) || {};
+  if (settings.hideDisconnected)
+    return false;
+  return true;
 }};
+WIDGETS["bluetooth"].width = WIDGETS["bluetooth"].show()?15:0;
 NRF.on('connect',WIDGETS["bluetooth"].changed);
 NRF.on('disconnect',WIDGETS["bluetooth"].changed);
