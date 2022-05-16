33f7173e8142723262d30b09d58df1e6998f134a
Iakov Davydov <idavydov@users.noreply.github.com>
2022-05-16 18:59:21 +0200

    widtest update

--- apps/widtest/widget.js
+++ apps/widtest/widget.js
@@ -1,1 +1,11 @@
-console.log("hello");
+(() => {
+  function draw() {
+    g.reset();
+    g.drawString("X", this.x, this.y);
+  }
+  WIDGETS["mywidget"]={
+    area:"tl",
+    width: 28,
+    draw:draw
+  };
+})()
